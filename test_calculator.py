6f04ede2bc4a163a0d512a27fc8d2c9729d99ea9
Kleditzl <Logan@kleditz.com>
2021-02-04 08:44:52 -0800

    Added more testing cases

--- test_calculator.py
+++ test_calculator.py
@@ -5,10 +5,13 @@ import calculator
 class TestCalculator(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(calculator.addition(3,4), 7)
+        self.assertEqual(calculator.addition(-3,-4), -7)       
     def test_subtraction(self):
         self.assertEqual(calculator.subtraction(4, 1), 3)
+        self.assertEqual(calculator.subtraction(-4, -1), -3)
     def test_multiplication(self):
         self.assertEqual(calculator.multiplication(2,3), 6)
+        self.assertEqual(calculator.multiplication(.5,.5), .25)
     def test_division(self):
         self.assertEqual(calculator.division(4, 1), 4)
         self.assertEqual(calculator.division(4, 0), 0)
